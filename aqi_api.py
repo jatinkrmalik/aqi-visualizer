aae8d71ec36f17635b2ab8f645d24435a2ed5e21
Jatin K Malik <jmalik@atlassian.com>
2023-11-11 23:54:19 +0530

    Enhance CLI with optional city detection and refine image filenames
    This PR brings a set of enhancements to the AQIVisualizer's command-line interface and image processing capabilities. The updates include new functionality and improvements to user experience and file management.
    
    Changes introduced:
    - The `--city` argument is now optional. If omitted, the tool defaults to 'here', auto-detecting the user's city based on their IP address.
    - A new `--aqi` flag has been added. Users can now choose whether to overlay the AQI on the generated image. By default, AQI data is included, but it can be disabled by setting `--aqi=false`.
    - Filenames and text overlays are now sanitized to include only English characters, and any trailing whitespace is removed. This ensures filenames are consistent and avoids issues with file systems that may not support extended character sets.
    
    Example improvements:
    - Filename "New Delhi US Embassy, India (नई दिल्ली अमेरिकी दूतावास)_180.png  " will now be saved as "New Delhi US Embassy, India_180.png".
    - Running the tool without specifying a city name will automatically use the user's current location for image generation.
    
    These updates are part of our commitment to improving usability and accessibility for all users of the AQIVisualizer.

--- aqi_api.py
+++ aqi_api.py
@@ -1,15 +1,22 @@
 import logging
 import requests
+from utils import filter_english_characters
+
 
 def get_aqi_data(city_name, token):
     logging.info(f"Fetching AQI data for {city_name}")
     aqi_url = f"https://api.waqi.info/feed/{city_name}/"
-    params = {'token': token}
+    params = {"token": token}
     response = requests.get(aqi_url, params=params)
     if response.status_code == 200:
         data = response.json()
         if data["status"] == "ok":
-            logging.info(f"Successfully retrieved AQI data: {data['data']['aqi']}")
-            return data["data"]["aqi"]
+            logging.info(
+                f"Successfully retrieved AQI data: {data['data']['aqi']} for city: {data['data']['city']['name']}"
+            )
+            if city_name == "here":
+                city_name = filter_english_characters(data["data"]["city"]["name"])
+            return city_name, data["data"]["aqi"]
+
     logging.error(f"Failed to retrieve data, status code: {response.status_code}")
     response.raise_for_status()
--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
 import logging
+import re
+
 
 # Setup logging
 def setup_logging():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+# Filter out non-English characters and strip whitespace from text ends
+def filter_english_characters(text):
+    # This regex pattern matches any character that is not a basic English letter (both cases), digit, common punctuation, or space
+    pattern = re.compile("[^a-zA-Z0-9,._\- ]+")
+    filtered_text = re.sub(pattern, "", text)
+    return filtered_text.strip()
