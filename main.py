6cd600cfeb02d38519ed79f59243105777c7d70b
Jatin K Malik <jmalik@atlassian.com>
2023-11-13 23:36:57 +0530

    Add Custom Font Face and Size Options for Text Overlay in AQI Visualizer

--- main.py
+++ main.py
@@ -18,32 +18,34 @@ def main():
 
     OPEN_AI_TOKEN = os.getenv("OPEN_AI_TOKEN")
     AQICN_TOKEN = os.getenv("AQICN_TOKEN")
-    FONT_PATH = "/System/Library/Fonts/Avenir Next.ttc"
-    FONT_SIZE = 50
 
     city_name = args.city
     custom_text = args.text
+    font_face = args.font_face
+    font_size = args.font_size
+
     output_path = args.output or os.getcwd()
 
     try:
         # Fetch AQI data if the --aqi flag is True
-        aqi = None
+        city_aqi = None
         if args.aqi:
-            city_name, aqi = get_aqi_data(city_name, AQICN_TOKEN)
-            logging.info(f"Retrieved AQI data: {aqi}")
+            city_name, city_aqi = get_aqi_data(city_name, AQICN_TOKEN)
+            logging.info(f"Retrieved AQI data: {city_aqi}")
 
         # Generate image prompt with or without AQI
-        prompt = generate_image_prompt(city_name, aqi)
+        prompt = generate_image_prompt(city_name, city_aqi)
 
         image_url = create_image_with_openai(OPEN_AI_TOKEN, prompt)
         image = download_image(image_url)
+
         # If the --aqi flag is True, add the AQI overlay to the image
         if args.aqi:
             image = add_text_to_image(
-                image, city_name, aqi, FONT_PATH, FONT_SIZE, custom_text
+                image, city_name, city_aqi, font_face, font_size, custom_text
             )
-        output_file_path = os.path.join(output_path, f"{city_name}_{aqi}.png")
-        save_and_copy_image(image, city_name, aqi, output_file_path)
+        output_file_path = os.path.join(output_path, f"{city_name}_{city_aqi}.png")
+        save_and_copy_image(image, city_name, city_aqi, output_file_path)
     except Exception as e:
         logging.error(f"An error occurred: {e}")
 
